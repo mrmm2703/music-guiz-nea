e1436f687fa441c8a8291a6b3b7f1141d4ca3bb6
mrmm2703 <35401592+mrmm2703@users.noreply.github.com>
2019-02-22 22:00:55 +0000

    Some more validation
    Added the ability to check the name of the track and take away parts of the song name that include things like "feat." or "with".

--- Development/databaseDownloader.py
+++ Development/databaseDownloader.py
@@ -36,7 +36,19 @@ with open("database.csv", mode="wb") as csvFile:
         song_response = unirest.get("https://api.spotify.com/v1/playlists/4ILMkgvpV7YFJX62MKQPfm/tracks?limit=100&offset=" + str(offset), headers={"Authorization": "Bearer " + token})
         for item in song_response.body['items']:
             try:
-                writer.writerow([item['track']['artists'][0]['name'], item['track']['name']])
+				# Skip acoustic and remixes because the original songs are already in the playlist
+				if "remix" in str(item['track']['name']).lower() or "acoustic" in str(item['track']['name']).lower():
+					continue
+				# Now remove the "(feat..." or "(with...)" parts of the title
+				if str(item['track']['name']).find('(feat') != -1:
+					fixedName = str(item['track']['name'])[0:str(item['track']['name']).find('(feat')-1]
+					writer.writerow([item['track']['artists'][0]['name'], fixedName])
+					continue
+				elif str(item['track']['name']).find('(with') != -1:
+					fixedName = str(item['track']['name'])[0:str(item['track']['name']).find('(with')-1]
+					writer.writerow([item['track']['artists'][0]['name'], fixedName])
+					continue
+				writer.writerow([item['track']['artists'][0]['name'], item['track']['name']])
             except:
                 pass
         # Add 100 to the offset to get the next set of songs
--- Development/main.py
+++ Development/main.py
@@ -4,38 +4,38 @@ import csv
 
 # Initialise variables
 points = 0
-password = ""
+password = "ele"
 
 def clean(song):
-        cleaned = song.lower()
-        cleaned = cleaned.strip()
-        cleaned = re.sub(" +", " ", cleaned)
-        return str(cleaned)
+    cleaned = song.lower()
+    cleaned = cleaned.strip()
+    cleaned = re.sub(" +", " ", cleaned)
+    return str(cleaned)
 
 def songNameFormat(song):
-        finalString = ""
-        words = song.split()
-        for word in words:
-                ind = 1
-                for letter in word:
-                        if(ind == 1):
-                                finalString = finalString + letter
-                        else:
-                                finalString = finalString + "_"
-                        ind = ind + 1
-                finalString = finalString + " "
-        return finalString
+	finalString = ""
+	words = song.split()
+	for word in words:
+		ind = 1
+		for letter in word:
+			if(ind == 1):
+				finalString = finalString + letter
+			else:
+				finalString = finalString + "_"
+			ind = ind + 1
+		finalString = finalString + " "
+	return finalString
 
 # Ask for password until correct
 while(True):
-	password_attempt = input("Enter the password: ")
+	password_attempt = raw_input("Enter the password: ")
 	if(password_attempt == password):
 		break # Break out of loop
 	else:
 		print("Incorrect password. Try again.")
 
 # Open CSV file and load into list 'songDB'
-file = open("songDatabase.csv", "rt")
+file = open("database.csv", "rt")
 fileReader = csv.reader(file)
 songDB = []
 
@@ -43,29 +43,29 @@ for row in fileReader:
 	songDB.append(row)
 
 while(True):
-        song = []
-        randInd = random.randint(0, len(songDB)-1)
-        song.append(songDB[randInd][0])
-        song.append(songDB[randInd][1])
-        tries = 1
-        print(songNameFormat(song[1]))
-        print("By: " + str(song[0]))
-        while(True):
-                songAttempt = input("Enter the full song name: ")
-                songAttempt = clean(songAttempt)
-                if(songAttempt == clean(song[1])):
-                        if(tries == 1):
-                                points = points + 3
-                                print("Points: " + str(points))
-                                break
-                        elif(tries == 2):
-                                points = points + 1
-                                print("Points: " + str(points))
-                                break
-                else:
-                        print("Incorrect answer.")
-                        tries = tries + 1
-                        if(tries > 2):
-                                endGame()
-                        else:
-                                pass
+    song = []
+    randInd = random.randint(0, len(songDB)-1)
+    song.append(songDB[randInd][0])
+    song.append(songDB[randInd][1])
+    tries = 1
+    print(songNameFormat(song[1]))
+    print("By: " + str(song[0]))
+    while(True):
+        songAttempt = raw_input("Enter the full song name: ")
+        songAttempt = clean(songAttempt)
+        if(songAttempt == clean(song[1])):
+            if(tries == 1):
+                points = points + 3
+                print("Points: " + str(points))
+                break
+            elif(tries == 2):
+                points = points + 1
+                print("Points: " + str(points))
+                break
+        else:
+            print("Incorrect answer.")
+            tries = tries + 1
+            if(tries > 2):
+                endGame()
+            else:
+                pass
