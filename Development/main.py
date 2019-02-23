618d8ff2cc911bd4ed46432cf1a35cdf1b23748e
mrmm2703 <35401592+mrmm2703@users.noreply.github.com>
2019-02-23 15:01:02 +0000

    Secure password entry
    You can't see the password when entering it now.

--- Development/main.py
+++ Development/main.py
@@ -1,3 +1,4 @@
+import getpass
 import re
 import random
 import csv
@@ -7,10 +8,10 @@ points = 0
 password = "ele"
 
 def clean(song):
-    cleaned = song.lower()
-    cleaned = cleaned.strip()
-    cleaned = re.sub(" +", " ", cleaned)
-    return str(cleaned)
+	cleaned = song.lower()
+	cleaned = cleaned.strip()
+	cleaned = re.sub(" +", " ", cleaned)
+	return str(cleaned)
 
 def songNameFormat(song):
 	finalString = ""
@@ -28,7 +29,7 @@ def songNameFormat(song):
 
 # Ask for password until correct
 while(True):
-	password_attempt = raw_input("Enter the password: ")
+	password_attempt = getpass.getpass()
 	if(password_attempt == password):
 		break # Break out of loop
 	else:
@@ -39,33 +40,40 @@ file = open("database.csv", "rt")
 fileReader = csv.reader(file)
 songDB = []
 
+# Put records from database into list
 for row in fileReader:
 	songDB.append(row)
 
 while(True):
-    song = []
-    randInd = random.randint(0, len(songDB)-1)
-    song.append(songDB[randInd][0])
-    song.append(songDB[randInd][1])
-    tries = 1
-    print(songNameFormat(song[1]))
-    print("By: " + str(song[0]))
-    while(True):
-        songAttempt = raw_input("Enter the full song name: ")
-        songAttempt = clean(songAttempt)
-        if(songAttempt == clean(song[1])):
-            if(tries == 1):
-                points = points + 3
-                print("Points: " + str(points))
-                break
-            elif(tries == 2):
-                points = points + 1
-                print("Points: " + str(points))
-                break
-        else:
-            print("Incorrect answer.")
-            tries = tries + 1
-            if(tries > 2):
-                endGame()
-            else:
-                pass
+	# Select random song and put into list
+	song = []
+	randInd = random.randint(0, len(songDB)-1)
+	song.append(songDB[randInd][0])
+	song.append(songDB[randInd][1])
+	songDB.pop(randInd)
+	tries = 1
+	print(songNameFormat(song[1]))
+	print("By: " + str(song[0]))
+	while(True):
+		songAttempt = raw_input("Enter the full song name: ")
+		songAttempt = clean(songAttempt)
+		while(songAttempt == ""):
+			print("You entered nothing. Try again: ")
+			songAttempt = raw_input("Enter the full song name: ")
+			songAttempt = clean(songAttempt)
+		if(songAttempt == clean(song[1])):
+			if(tries == 1):
+				points = points + 3
+				print("Points: " + str(points))
+				break
+			elif(tries == 2):
+				points = points + 1
+				print("Points: " + str(points))
+				break
+		else:
+			print("Incorrect answer.")
+			tries = tries + 1
+			if(tries > 2):
+				endGame()
+			else:
+				pass
